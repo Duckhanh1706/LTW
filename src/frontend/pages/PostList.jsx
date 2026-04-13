61b7de9e1eb1e3f6a63044a2839b3f8cffa8f2d1
Duckhanh1706 <nguyenduckhanh17062004@gmail.com>
2026-04-13 15:49:30 +0700

    bai tap 13/4

---
+++ src/frontend/pages/PostList.jsx
@@ -0,0 +1,29 @@
+// frontend/src/components/BlogList.jsx
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const PostLists = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/blogs")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data))
+      .catch((error) => console.error("Error fetching blogs:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog List</h1>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.slug}>
+            <Link to={`/posts/${blog.slug}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostLists;
